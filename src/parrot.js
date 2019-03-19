76fea0d106b1447c18072a3f7e6ad678402e2d9d
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-03-19 06:15:47 -0400

    Refactor type const

--- src/parrot.js
+++ src/parrot.js
@@ -5,34 +5,23 @@ export const PARROT_TYPES = {
 };
 
 export class Parrot {
-    constructor(type, numberOfCoconuts, voltage, isNailed) {
+    constructor(numberOfCoconuts, voltage, isNailed) {
         this.loadFactor = 9;
         this.baseSpeed = 12;
 
-        this.type = type;
         this.numberOfCoconuts = numberOfCoconuts;
         this.voltage = voltage;
         this.isNailed = isNailed;
     }
 
     getSpeed() {
-        switch (this.type) {
-
-            case PARROT_TYPES.EUROPEAN:
-                return new European(this.numberOfCoconuts, this.voltage, this.isNailed).getSpeed();
-            case PARROT_TYPES.AFRICAN:
-                return new African(this.numberOfCoconuts, this.voltage, this.isNailed).getSpeed();
-            case PARROT_TYPES.NORWEGIAN_BLUE:
-                return new Norwegian_Blue(this.numberOfCoconuts, this.voltage, this.isNailed).getSpeed();
-        }
-        throw new Error("Should be unreachable");
     }
 
 }
 
-class African extends Parrot{
+export class African extends Parrot{
     constructor(numberOfCoconuts, voltage, isNailed) {
-        super("AFRICAN",numberOfCoconuts, voltage, isNailed);
+        super(numberOfCoconuts, voltage, isNailed);
     }
 
     getSpeed() {
@@ -40,9 +29,9 @@ class African extends Parrot{
     }
 }
 
-class Norwegian_Blue extends Parrot{
+export class Norwegian_Blue extends Parrot{
     constructor(numberOfCoconuts, voltage, isNailed) {
-        super("NORWEGIAN",numberOfCoconuts, voltage, isNailed);
+        super(numberOfCoconuts, voltage, isNailed);
     }
 
     getSpeed() {
@@ -53,9 +42,9 @@ class Norwegian_Blue extends Parrot{
     }
 }
 
-class European extends Parrot{
+export class European extends Parrot{
     constructor(numberOfCoconuts, voltage, isNailed) {
-        super("EUROPEAN",numberOfCoconuts, voltage, isNailed);
+        super(numberOfCoconuts, voltage, isNailed);
     }
 
     getSpeed() {
